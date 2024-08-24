ed449fe29606ef68a24c9cbde9362c61992b17e3
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-08-24 02:31:18 +0200

    deleted param

--- backend/src/handlers/health.handler.ts
+++ backend/src/handlers/health.handler.ts
@@ -1,9 +1,8 @@
-import { NextFunction, RequestHandler, Request, Response } from "express";
+import { RequestHandler, Request, Response } from "express";
 
 export const handleHealthCheck: RequestHandler = async (
   _: Request,
   res: Response,
-  __: NextFunction,
 ) => {
   return res.send("OK").status(200);
 };
