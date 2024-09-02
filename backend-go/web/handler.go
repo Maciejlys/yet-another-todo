7bb567d801fe432e19791ebafde4d969ec74d410
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-09-02 19:42:49 +0200

    add logger

--- backend-go/web/handler.go
+++ backend-go/web/handler.go
@@ -3,6 +3,7 @@ package web
 import (
 	"github.com/Maciejlys/yet-another-todo"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type Handler struct {
@@ -15,6 +16,8 @@ func NewHandler(store todo.Store) *Handler {
 		Mux:   chi.NewMux(),
 		store: store,
 	}
+
+	h.Use(middleware.Logger)
 
 	todos := TodoHandler{store: store}
 
